3661d5c2781597b95f805c6faf091062bbaa4e59
NrcNLy <conley.nicholasr@gmail.com>
2025-05-29 07:51:12 -0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'my-pwa-cache-v1';
+const urlsToCache = [
+  './', // Caches the root of your site
+  './index.html',
+  './manifest.json',
+  './icon-monk.png' // Corrected icon file name here
+];
+
+// Install event: cache files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event: serve from cache if available, otherwise fetch from network
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+// Activate event: clean up old caches
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
